65d3eb82bd2d9a7cb9583b35a9ef34ddb62a7bd5
chanw12 <wooju9812127@gmail.com>
2023-10-30 16:07:51 +0900

    종료 기능 구현 (#2)
    * 테스트 유틸 생성
    
    * 명령어 입력창구현
    
    * 명령어 입력확인
    
    * 종료 기능 구현

--- src/test/java/com/ll/TestMain.java
+++ src/test/java/com/ll/TestMain.java
@@ -1,5 +1,50 @@
 package com.ll;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+
 public class TestMain {
 
+    @Test
+    @DisplayName("명령어 입력창 구현")
+    void t1(){
+        ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
+
+
+        System.out.print("명령)");
+        String out = byteArrayOutputStream.toString().trim();
+        Assertions.assertThat(out).isEqualTo("명령)");
+    }
+
+    @Test
+    @DisplayName("명령어 입력 확인")
+    void t2(){
+        ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
+        System.out.print("명령)");
+        String out = byteArrayOutputStream.toString().trim();
+        Scanner scanner = TestUtil.genScanner("종료");
+        String cmd = scanner.nextLine();
+
+        Assertions.assertThat(cmd).isEqualTo("종료");
+    }
+
+    @Test
+    @DisplayName("종료 기능 확인")
+    void t3(){
+
+        ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
+
+        Scanner scanner = TestUtil.genScanner("""
+                종료
+                """.stripIndent());
+        App app = new App(scanner);
+
+        app.run(); // 프로그램 실행
+
+
+    }
 }
